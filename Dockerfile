673c4fc2487377c756bf26b7d8eb0f243d273f61
gokceyucel <gokce.yucel@emlakjet.com>
2018-09-28 22:01:10 +0300

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
 FROM node:8.12.0-alpine as react-build
-
-# Install app dependencies
-COPY package.json /www/package.json
-RUN cd /www; npm install --silent
-
-# Set work directory to /www
-WORKDIR /www
-
-# start command as per package.json
-CMD ["npm", "run", "build:prod"]
+ARG API_URL
+ARG API_PORT
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . ./
+RUN npm run build:prod
 
 FROM nginx:alpine
-COPY --from=react-build /www /usr/share/nginx/html
+COPY --from=react-build /app/dist /usr/share/nginx/html
 EXPOSE 9090
 CMD ["nginx", "-g", "daemon off;"]
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,35 @@
-const path = require("path");
-const Dotenv = require('dotenv-webpack');
+var path = require('path');
+var Dotenv = require('dotenv-webpack');
 
-var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC_DIR = path.resolve(__dirname, "src");
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
 
-// const env = dotenv.config().parsed;
-// const envKeys = Object.keys(env).reduce((prev, next) => {
-//     prev[ `process.env.${next}` ] = JSON.stringify(env[ next ]);
-//     return prev;
-// }, {});
-
-var config = {
-    entry: SRC_DIR + "/app/index.js",
-    output: {
-        path: DIST_DIR + "/app",
-        filename: "bundle.js",
-        publicPath: "/app/"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: [ "react", "es2015", "stage-2" ]
-                }
-            },
-            {
-                test: /\.css$/,
-                loaders: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new Dotenv({
-            systemvars: true
-        })
+module.exports = {
+  entry: SRC_DIR + '/app/index.js',
+  output: {
+    path: DIST_DIR + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: [ 'react', 'es2015', 'stage-2' ]
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [ 'style-loader', 'css-loader' ]
+      }
     ]
+  },
+  plugins: [
+    new Dotenv({
+      systemvars: true
+    })
+  ]
 };
-
-module.exports = config;
