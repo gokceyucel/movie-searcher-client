0a65458cf74576477d903f1db7bffb8e36b9263a
Gokce Yucel <gkcycl@gmail.com>
2019-01-07 13:19:47 +0300

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,23 @@ var DIST_DIR = path.resolve(__dirname, 'dist');
 var SRC_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: ['babel-polyfill', SRC_DIR + '/app/index.js'],
+  mode: 'production',
+  entry: ['@babel/polyfill', SRC_DIR + '/app/index.js'],
   output: {
     path: DIST_DIR + '/app',
     filename: 'bundle.js',
     publicPath: '/app/'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js?/,
         include: SRC_DIR,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2']
-        }
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
